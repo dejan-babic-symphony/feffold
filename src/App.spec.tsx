3b98dfae186ccd1edda1cffc8cb90fcf122bff26
Dejan Babic <dejan.babic@symphony.is>
2024-12-21 11:11:21 +0100

    chore: run prettier format

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import { App } from "./App"
+import { App } from './App';
 
 describe('App', () => {
   it('renders the App text', () => {
-    render(<App />)
-    expect(screen.getByText('App')).toBeInTheDocument()
-  })
-})
+    render(<App />);
+    expect(screen.getByText('App')).toBeInTheDocument();
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
-import { defineConfig as defineViteConfig, mergeConfig } from 'vite'
-import { defineConfig as defineVitestConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig as defineViteConfig, mergeConfig } from 'vite';
+import { defineConfig as defineVitestConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 const viteConfig = defineViteConfig({
   plugins: [react()],
-
-})
+});
 
 const vitestConfig = defineVitestConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './vitest.setup.ts'
+    setupFiles: './vitest.setup.ts',
   },
-}) 
+});
 
-export default mergeConfig(viteConfig, vitestConfig)
+export default mergeConfig(viteConfig, vitestConfig);
